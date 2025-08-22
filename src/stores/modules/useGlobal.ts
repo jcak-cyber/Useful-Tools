db0b6b6f44292746e246e1529125abe6feafa5ba
zzy <zz13698161685@163.com>
2025-08-22 11:41:30 +0800

    fix: 细节优化

--- src/stores/modules/useGlobal.ts
+++ src/stores/modules/useGlobal.ts
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-// 这只是一个测试例子，请改成你自己的文件存储逻辑
 export const useGlobalStore = defineStore("test", () => {
   const canEditStorage = ref<boolean>(false);
 
